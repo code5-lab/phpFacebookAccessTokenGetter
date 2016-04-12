bb0105ecdc50666130285caef8467a8c182ca34b
ist170542 <duarte.baptista@ist.utl.pt>
2016-04-12 18:12:19 +0100

    modified user interface shows page token, id for each page(instead of the first page of the list)

--- facebookLogin.php
+++ facebookLogin.php
@@ -15,20 +15,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 		}
 
         if(!isset($_POST['appID'])){
-
             $APP_ID = test_input($_SESSION['appID']);
             $APP_SECRET = test_input($_SESSION['app_secret']);
             $DEFAULT_VERSION = test_input($_SESSION['default_graph_version']);
-
         }else{
-
             $APP_ID = test_input($_POST["appID"]);
             $APP_SECRET = test_input($_POST['appSecret']);
             $DEFAULT_VERSION = test_input($_POST['defVersion']);
-
         }
-
-//putenv("APP_ID_ENV_NAME=$APP_ID");
 
 $fb = new Facebook\Facebook([
     'app_id' => $APP_ID,
@@ -38,7 +32,6 @@ $fb = new Facebook\Facebook([
 
 if(isset($_SESSION['fb_access_token'])) {
 
-    echo "is set";
     $accessToken = $_SESSION['fb_access_token'];
 
 } else {
@@ -72,29 +65,24 @@ if(isset($accessToken)) {
     $_SESSION['fb_access_token'] = (string) $accessToken;
 
     try {
+
+        $response = $fb->get('/me/accounts', $accessToken);                     // requests info on user's managed pages
+
+        //$pageToken = $response->getDecodedBody()['data'][0]["access_token"];    // page token
+        //$pageId = $response->getDecodedBody()['data'][0]["id"];                 // id of the first page
+
+        foreach($response->getDecodedBody()['data'] as $pageRetrieved){           // for each page retrieved prints
+            echo '<pre>';                                                         //id and token
+                print_r("Page id: ".$pageRetrieved["id"]);
+                print_r("Page token: ".$pageRetrieved["access_token"]);
+            echo "<br>";
+            echo '</pre>';
+        }
+
+        /*$responseFeed = $fb->get("/{$pageId}/feed", $pageToken);                //requests page's last posts
+                                                                                  // (limit=default=100)
 /*
-        $response = $fb->get('/me/accounts', $accessToken);
-
-      //  print_r($response->getAccessToken());
-       // var_dump($response->getDecodedBody()['data'][0]["access_token"]);
-        $pageToken = $response->getDecodedBody()['data'][0]["access_token"];
-        $pageId = $response->getDecodedBody()['data'][0]["id"];
-
-        echo '<pre>';
-        print_r($response->getDecodedBody());
-        echo '</pre>';
-
-        print_r("/{$pageId}/feed");
-
-        $responseFeed = $fb->get("/{$pageId}/feed", $pageToken);
-
-
-        echo '<pre>';
-        print_r($responseFeed->getDecodedBody());
-        echo '</pre>';
-
-
-        $responseFeed = $fb->post("/{$pageId}/feed", array(
+        $responseFeed = $fb->post("/{$pageId}/feed", array(                     //issues facebook post to that page
             'message' => 'postado atraves da app!!'
         ), $pageToken);
 */
